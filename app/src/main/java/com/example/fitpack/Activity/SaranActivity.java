5f1aee1dfdb6c7ab1450ba887c61dd511e7ea989
alfisyar520 <alfisyarpranata@gmail.com>
2020-08-16 17:40:12 +0700

    isi fragment

--- app/src/main/java/com/example/fitpack/Activity/SaranActivity.java
+++ app/src/main/java/com/example/fitpack/Activity/SaranActivity.java
@@ -1,17 +1,72 @@
 package com.example.fitpack.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.example.fitpack.Model.User;
 import com.example.fitpack.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SaranActivity extends AppCompatActivity {
+
+    private TextView tv_name, tv_tanggal;
+    private ImageView iv_profile;
+    private String mUser;
+
+    //firebase
+    private FirebaseUser firebaseUser;
+    private DatabaseReference reference;
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saran);
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser().getUid();
+
+        tv_name = findViewById(R.id.tv_saran_name);
+        tv_tanggal = findViewById(R.id.tv_saran_tanggal);
+        iv_profile = findViewById(R.id.image_saran_profile);
+
+        Toolbar toolbar = findViewById(R.id.topBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                tv_name.setText(user.getUsername());
+                Glide.with(SaranActivity.this).load(user.getImageUrl()).into(iv_profile);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
 
     }
